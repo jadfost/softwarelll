0bf4aef3938d7a1347fde798aabe6500a17d1fa2
stevenorduz <stevenorduz@hotmail.com>
2022-11-27 17:16:05 -0500

    Commit 3.8 Configuraciones

--- app/src/main/java/com/example/softwarelll/Confirmacion.java
+++ app/src/main/java/com/example/softwarelll/Confirmacion.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,29 +22,21 @@ public class Confirmacion extends AppCompatActivity {
 
         final Button confimarBtn = findViewById(R.id.confirmBtn);
         final Button cancelarBtn = findViewById(R.id.backBtn);
-        TextView totall = findViewById(R.id.txttotal);
         final EditText direccion = findViewById(R.id.direccionedit);
 
-        Intent intents = getIntent();
-        if(intents.hasExtra(Intent.EXTRA_TEXT)) {
-            // Notas es mi entity
-            totall = (TextView) intents.getSerializableExtra(Intent.EXTRA_TEXT);
-        }
         cancelarBtn.setOnClickListener(view -> {
-            Intent listaProductos = new Intent(getBaseContext(), ListaProductos.class);
-            startActivity(listaProductos);
-
+            Intent lproduct = new Intent(getBaseContext(), ListaProductos.class);
+            startActivity(lproduct);
         });
 
         confimarBtn.setOnClickListener(view -> {
-            AlertDialog.Builder alertdialog = new AlertDialog.Builder(this);
+            AlertDialog.Builder alertdialog = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
             alertdialog.setMessage("Tu pedido sera enviado a la direccion: " + direccion.getText().toString()
                     + ", llegara en Aproximandamente 30-50 Minutos. Desea Confirmar?")
                     .setCancelable(false)// Aqui defines que no se pueda dar atras (cancelar el dialog) el usuario debera seleccionar una opcion
                     .setPositiveButton(" Si", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            // agrega aqui la funcion que quieres hacer con el boton positivo
                         }
                     })
                     .setNegativeButton("No", new DialogInterface.OnClickListener() {
